19be92cd0054182077aae5f6536ff2d0a65d37fb
noobfornow <farihaanjumaupy@gmail.com>
2025-03-08 01:45:29 +0600

    minor fix

--- 02_basics/06_functions.js
+++ 02_basics/06_functions.js
@@ -11,4 +11,4 @@ user.greeting = function () {
   console.log(`Hello, ${this.name}`);
 };
 console.log(user.greeting());
-// The output is "Hello, Aupy undefined" because the function `greeting` does not return anything. To fix this, we can add a return statement to the function:
+// The output is "Hello, Aupy undefined" because the function `greeting` does not return anything. To fix this, we can add a return statement to the function: return `Hello, ${this.name}`; like this
