8cdc3cba6ef6f345cf31014fd50de6070323dd12
noobfornow <farihaanjumaupy@gmail.com>
2025-03-07 15:51:15 +0600

    first commit

--- data-types.js
+++ data-types.js
@@ -2,3 +2,16 @@
 
 //alert(3+3) //we are not allowed to use alert in node js, it is only for browser
 
+let name = "Aupy" //string
+let age = 21    //number
+let isApproved = true   //boolean
+
+//null is used to clear the value of a variable
+let firstName = null    //it means that the value of the variable is empty
+
+//symbol is used to create unique identifiers for objects
+//object 
+
+console.log(typeof name)
+console.log(typeof null) //it will show object but it is a bug in js
+console.log(typeof undefined) //undefined is a type in js
